d553a24c698b95f3ef4adf76c64f08bbbdae7773
badriana400952 <badriana0402@gmail.com>
2023-06-16 02:32:37 +0700

    day 15

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"go-latihan1/connection"
@@ -10,7 +11,10 @@ import (
 	"strconv"
 	"text/template"
 
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // struct => hampir mirip seperti object di javascript
@@ -28,14 +32,32 @@ type Blog struct {
 	Img        string
 }
 
+type User struct {
+	ID       int
+	Name     string
+	Email    string
+	Password string
+}
+
+type SessionData struct {
+	IsLogin bool
+	Name    string
+}
+
+var userData = SessionData{}
+
 func main() {
 	connection.DatabaseConnect()
+
 	e := echo.New()
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("session"))))
 	// e.GET("/hehe", func(c echo.Context) error {
 	// 	return c.String(http.StatusOK, "Hello, World!")
 	// })
 	e.Static("/public", "public")
+
 	e.Static("/view", "view")
+
 	e.GET("rumah", halloWord)
 	e.GET("hallo", hallo)
 	e.GET("/", home)
@@ -45,10 +67,18 @@ func main() {
 	e.GET("/myEdit/:id", editBlog)
 	e.GET("/detail/:id", detail)
 
+	e.GET("/formlogin", formlogin)
+	e.GET("/regis", regis)
+
 	e.POST("/myEdit/:id", metodBlogEdit)
+	e.POST("/register", register)
 
 	e.POST("tambahBlog", tambahBlog)
 	e.POST("/deleteBlog/:id", deleteBlog)
+
+	e.POST("/login", login)
+
+	e.POST("/logout", logout)
 	e.Logger.Fatal(e.Start("localhost:3000"))
 
 }
@@ -61,12 +91,31 @@ func hallo(c echo.Context) error {
 }
 
 func home(c echo.Context) error {
+
+	sess, _ := session.Get("session", c)
+
+	if sess.Values["isLogin"] != true {
+		userData.IsLogin = false
+	} else {
+		userData.IsLogin = sess.Values["isLogin"].(bool)
+		userData.Name = sess.Values["name"].(string)
+	}
+
+	datas := map[string]interface{}{
+		"FlashStatus":  sess.Values["status"],
+		"FlashMessage": sess.Values["message"],
+		"DataSession":  userData,
+	}
+
+	delete(sess.Values, "message")
+	delete(sess.Values, "status")
+	sess.Save(c.Request(), c.Response())
+
 	var template, err = template.ParseFiles("view/index.html")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"massage": err.Error()})
 	}
-	// jalankan response template
-	return template.Execute(c.Response(), nil)
+	return template.Execute(c.Response(), datas) //datas harusnya
 }
 
 func myBlog(c echo.Context) error {
@@ -126,14 +175,25 @@ func blogview(c echo.Context) error {
 		}
 		hasil = append(hasil, each)
 	}
+
+	sess, _ := session.Get("session", c)
+
+	if sess.Values["isLogin"] != true {
+		userData.IsLogin = false
+	} else {
+		userData.IsLogin = sess.Values["isLogin"].(bool)
+		userData.Name = sess.Values["name"].(string)
+	}
+
+	//
+	blogs := map[string]interface{}{
+		"Blogs":       hasil,
+		"DataSession": userData,
+	}
 	var temp, err = template.ParseFiles("view/blogview.html")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"massage": err.Error()})
 	}
-
-	blogs := map[string]interface{}{
-		"Blogs": hasil}
-
 	return temp.Execute(c.Response(), blogs)
 }
 
@@ -250,4 +310,100 @@ func deleteBlog(c echo.Context) error {
 	}
 
 	return c.Redirect(http.StatusMovedPermanently, "/blogview")
+}
+
+func regis(c echo.Context) error {
+	var template, err = template.ParseFiles("view/regis.html")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"massage": err.Error()})
+	}
+	return template.Execute(c.Response(), nil)
+}
+func register(c echo.Context) error {
+	err := c.Request().ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	name := c.FormValue("inputName")
+	email := c.FormValue("inputEmail")
+	password := c.FormValue("inputPassword")
+
+	passwordHash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	_, err = connection.Conn.Exec(context.Background(), "INSERT INTO tb_user(name, email, password) VALUES ($1, $2, $3)", name, email, passwordHash)
+
+	if err != nil {
+		redirectWithMessage(c, "Register failed, please try again.", false, "/form-register")
+	}
+
+	return redirectWithMessage(c, "Register success!", true, "/formlogin")
+
+}
+func formlogin(c echo.Context) error {
+
+	sess, _ := session.Get("session", c)
+
+	flash := map[string]interface{}{
+		"FlashStatus":  sess.Values["status"],
+		"FlashMessage": sess.Values["message"],
+	}
+
+	delete(sess.Values, "message")
+	delete(sess.Values, "status")
+	sess.Save(c.Request(), c.Response())
+
+	var tmpl, err = template.ParseFiles("view/login.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), flash)
+}
+
+func login(c echo.Context) error {
+	err := c.Request().ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	email := c.FormValue("inputEmail")
+	password := c.FormValue("inputPassword")
+
+	user := User{}
+	err = connection.Conn.QueryRow(context.Background(), "SELECT * FROM tb_user WHERE email=$1", email).Scan(&user.ID, &user.Name, &user.Email, &user.Password)
+	if err != nil {
+		return redirectWithMessage(c, "Email Incorrect!", false, "/formlogin")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return redirectWithMessage(c, "Password Incorrect!", false, "/formlogin")
+	}
+
+	sess, _ := session.Get("session", c)
+	sess.Options.MaxAge = 10800 // 3 JAM
+	sess.Values["message"] = "Login success!"
+	sess.Values["status"] = true
+	sess.Values["name"] = user.Name
+	sess.Values["email"] = user.Email
+	sess.Values["id"] = user.ID
+	sess.Values["isLogin"] = true
+	sess.Save(c.Request(), c.Response())
+
+	return c.Redirect(http.StatusMovedPermanently, "/")
+}
+func logout(c echo.Context) error {
+	sess, _ := session.Get("session", c)
+	sess.Options.MaxAge = -1
+	sess.Save(c.Request(), c.Response())
+
+	return c.Redirect(http.StatusMovedPermanently, "/")
+}
+
+func redirectWithMessage(c echo.Context, message string, status bool, path string) error {
+	sess, _ := session.Get("session", c)
+	sess.Values["message"] = message
+	sess.Values["status"] = status
+	sess.Save(c.Request(), c.Response())
+	return c.Redirect(http.StatusMovedPermanently, path)
 }
